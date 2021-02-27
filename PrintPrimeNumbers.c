1d1e1d6ce3c46a395df9df31b367e20347812cdb
Onula <73027584+Onula@users.noreply.github.com>
2021-02-27 20:40:42 +0200

    Update PrintPrimeNumbers.c

--- PrintPrimeNumbers.c
+++ PrintPrimeNumbers.c
@@ -1,22 +1,23 @@
+//This code print the prime numbers  using isPrime(n) function
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <stdbool.h> //To be used the true and false
 
-bool isPrime(int n);
+bool isPrime(int n);// returns true or false
 
 int main(){
 	int i, n;
-	scanf("%d", &n);
+	scanf("%d", &n);//input(last number)
 	for(i=2; i<=n; i++) if( isPrime(i) ) printf("%d ", i);
 }
 
 
 bool isPrime(int n){
 	int i;
-	for(i=sqrt(n); i>1; i--) if(n%i == 0) return false;
+	for(i=sqrt(n); i>1; i--) if(n%i == 0) return false;//If at least one number between {2,...,sqrt(n)} divides the n number then it is not a prime number 
 	
-	return true;
+	return true;//if dont have a number between {2,...,sqrt(n)} that divides the n number then n it is prime number
 }
 
 
